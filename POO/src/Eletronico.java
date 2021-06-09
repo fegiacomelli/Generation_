66c6945373d3704f26fd97def26122ff2073fbdd
fegiacomelli <69607844+fegiacomelli@users.noreply.github.com>
2021-06-08 21:31:38 -0300

    atividade POO

---
+++ POO/src/Eletronico.java
@@ -0,0 +1,66 @@
+
+public class Eletronico {
+	
+	private String marca;
+	private String modelo;
+	private String cor;
+	private int capacidade;
+	private double valor;
+	
+	public Eletronico(String marca,String modelo,String cor,int capacidade,double valor)
+	{
+		this.marca = marca;
+		this.modelo = modelo;
+		this.cor = cor;
+		this.capacidade = capacidade;
+		this.valor = valor;
+	}
+	
+	public void InfoCell() 
+	{
+		System.out.println("\nMarca:"+marca+"\nModelo:"+modelo+"\nCor:"+cor+"\nCapacidade:"+capacidade+"Gb"+"\nValor: R$"+valor);
+	}
+
+	public String getMarca() {
+		return marca;
+	}
+
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+
+	public String getModelo() {
+		return modelo;
+	}
+
+	public void setModelo(String modelo) {
+		this.modelo = modelo;
+	}
+
+	public String getCor() {
+		return cor;
+	}
+
+	public void setCor(String cor) {
+		this.cor = cor;
+	}
+
+	public int getCapacidade() {
+		return capacidade;
+	}
+
+	public void setCapacidade(int capacidade) {
+		this.capacidade = capacidade;
+	}
+
+	public double getValor() {
+		return valor;
+	}
+
+	public void setValor(double valor) {
+		this.valor = valor;
+	}
+	
+	
+
+}
---
+++ POO/src/TesteContaBancaria.java
@@ -0,0 +1,11 @@
+
+public class TesteContaBancaria {
+
+	public static void main(String[] args) {
+		
+		contaBancaria conta1 = new contaBancaria("Felipe Giacome","Rua celso...",411779809,1051105,"Conta corrente",10000);
+		conta1.printC();
+
+	}
+
+}
